0d8503bfa663ac205a1fec958f061bc84f1314ff
TAEHYOUNG <109320678+wolfy916@users.noreply.github.com>
2024-01-12 20:05:11 +0900

    Title: 알파벳 쟁탈전, Time: 3425ms, Memory: 28MB, Status: Passed - CodeTree

--- 240112/알파벳 쟁탈전/alphabetics-competition.py
+++ 240112/알파벳 쟁탈전/alphabetics-competition.py
@@ -12,25 +12,32 @@ def input():
 
 # [B] 백트랙킹 함수
 def dfs(idx):
-    global answer, possible
+    global answer
     if idx > N:
         answer += 1
         return
     
     for v in range(3):
         if not possible[idx][v]: continue
-        tmp = deepcopy(possible)
+        tmp = []
         for k in range(3):
                 if v == k: continue
+                if not possible[idx][k]: continue
+                tmp.append((idx, k))
                 possible[idx][k] = False
         for s in rules[idx][1]:
             for k in range(3):
                 if v == k: continue
+                if not possible[s][k]: continue
+                tmp.append((s, k))
                 possible[s][k] = False
         for d in rules[idx][0]:
+            if not possible[d][v]: continue
+            tmp.append((d, v))
             possible[d][v] = False
         dfs(idx + 1)
-        possible = tmp
+        for t1, t2 in tmp:
+            possible[t1][t2] = True
 
 # [Main]
 if __name__ == "__main__":
@@ -44,7 +51,7 @@ if __name__ == "__main__":
             rules[b][cnd[c]].append(a)
         else:
             rules[a][cnd[c]].append(b)
-            
+
     possible = [[True] * 3 for _ in range(N + 1)]
     answer = 0
     dfs(1)

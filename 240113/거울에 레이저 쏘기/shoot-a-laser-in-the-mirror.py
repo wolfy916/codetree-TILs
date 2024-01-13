f62f89b3c9b3152633c84cdd03fe60a48a3bda3d
TAEHYOUNG <109320678+wolfy916@users.noreply.github.com>
2024-01-13 21:50:40 +0900

    Title: 거울에 레이저 쏘기, Time: 360ms, Memory: 34MB, Status: Passed - CodeTree

--- 240113/거울에 레이저 쏘기/shoot-a-laser-in-the-mirror.py
+++ 240113/거울에 레이저 쏘기/shoot-a-laser-in-the-mirror.py
@@ -8,18 +8,19 @@ import sys
 def input():
     return sys.stdin.readline().rstrip('\n')
 
-# [B] 카운팅 함수
-def count(i, j, k):
+# [B] 카운팅
+def count(i, j, prev):
     global answer
+    if visited[i][j]: return
+    visited[i][j] = True
     cnt = 0
     while 0 <= i < N and 0 <= j < M:
-        if visited[i][j]: return
-        visited[i][j] = True
         cnt += 1
-        di, dj = link[delta[k]][kind[board[i][j]]]
+        di, dj = link[prev][kind[board[i][j]]]
         i += di
         j += dj
-        k = delta.index((di, dj))
+        prev = (di, dj)
+    visited[i - di][j - dj] = True
     answer = max(answer, cnt)
 
 # [Main]
@@ -42,10 +43,10 @@ if __name__ == "__main__":
         if i < 1 or i >= N - 1:
             for j in range(M):
                 dir = 1 if i < 1 else 0
-                count(i, j, dir)
+                count(i, j, delta[dir])
         else:
             for j in [0, M - 1]:
-                dir = 3 if j > 0 else 2
-                count(i, j, dir)
+                dir = 2 if j > 0 else 3
+                count(i, j, delta[dir])
 
     print(answer)

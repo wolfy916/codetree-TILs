5330e69802e11ca397db14ad8dec01154e8740e8
TAEHYOUNG <109320678+wolfy916@users.noreply.github.com>
2024-01-14 14:47:23 +0900

    Title: 카드 사이클 만들기, Time: 146ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240114/카드 사이클 만들기/create-card-cycle.py
@@ -0,0 +1,31 @@
+'''
+카드 사이클 만들기 - 코드트리 실버3
+분류 : ?
+'''
+import sys
+
+# [A] 입력함수 초기화
+def input():
+    return sys.stdin.readline().rstrip('\n')
+
+# [Main]
+if __name__ == "__main__":
+    N = int(input())
+    nums = [int(input()) for _ in range(N)]
+    target = [int(input()) for _ in range(N)]
+
+    cnt, max_size = 0, -1
+    visited = [False] * N
+    for i in range(N):
+        if nums[i] == target[i]: continue
+        size = 1
+        nxt = target.index(nums[i])
+        while nxt != i:
+            nums[i], nums[nxt] = nums[nxt], nums[i]
+            nxt = target.index(nums[i])
+            size += 1
+        else:
+            cnt += 1
+            max_size = max(max_size, size)
+    
+    print(cnt, max_size)

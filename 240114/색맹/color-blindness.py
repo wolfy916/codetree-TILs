861839c4e34aea07e58a67fd7f8f258b351ae56e
TAEHYOUNG <109320678+wolfy916@users.noreply.github.com>
2024-01-14 15:39:56 +0900

    Title: 색맹, Time: 156ms, Memory: 25MB, Status: Wrong Answer - CodeTree

--- 240114/색맹/color-blindness.py
+++ 240114/색맹/color-blindness.py
@@ -9,38 +9,43 @@ from collections import deque as dq
 def input():
     return sys.stdin.readline().rstrip('\n')
 
+# [B] bfs
+def bfs(i, j):
+    global RG_cnt
+    visited[i][j] = num[board[i][j]]
+    queue = dq([(i, j)])
+    RG_flag = False
+    while queue:
+        vi, vj = queue.popleft()
+        for di, dj in delta:
+            ni, nj = vi + di, vj + dj
+            if ni < 0 or nj < 0 or ni >= N or nj >= N: continue
+            if board[ni][nj] != board[i][j]:
+                if visited[ni][nj] > 1:
+                    RG_flag = True
+                continue
+            if visited[ni][nj]: continue
+            visited[ni][nj] = num[board[i][j]]
+            queue.append((ni, nj))
+    if RG_flag and board[i][j] != 'B':
+        RG_cnt -= 1
+
 # [Main]
 if __name__ == "__main__":
     # [1] 데이터 입력 및 초기화
     N = int(input())
     board = [input() for _ in range(N)]
     total_cnt = RG_cnt = 0
-    nxt = {'R': ['R', 'G'], 'G': ['R', 'G'], 'B': ['B']}
-    visited = [[False] * N for _ in range(N)]
+    num = {'R': 3, 'G': 2, 'B': 1}
+    visited = [[0] * N for _ in range(N)]
     delta = ((-1, 0), (1, 0), (0, -1), (0, 1))
 
     # [2] bfs 탐색
     for i in range(N):
         for j in range(N):
             if visited[i][j]: continue
-            visited[i][j] = True
-            color = {'R': False, 'G': False, 'B': False}
-            color[board[i][j]] = True
-            queue = dq([(i, j)])
-            while queue:
-                vi, vj = queue.popleft()
-                for di, dj in delta:
-                    ni, nj = vi + di, vj + dj
-                    if ni < 0 or nj < 0 or ni >= N or nj >= N: continue
-                    if visited[ni][nj]: continue
-                    if board[ni][nj] not in nxt[board[i][j]]: continue
-                    color[board[ni][nj]] = True
-                    visited[ni][nj] = True
-                    queue.append((ni, nj))
-            
-            # 그룹 카운트
-            total_cnt += list(color.values()).count(True)
-            if color['R'] or color['G']: RG_cnt += 1
-            if color['B']: RG_cnt += 1
+            total_cnt += 1
+            RG_cnt += 1
+            bfs(i, j)
     
     print(total_cnt, RG_cnt)

7036cb9e5a688268f45f4b0d554568a4be4ddb28
TAEHYOUNG <109320678+wolfy916@users.noreply.github.com>
2024-01-22 14:32:48 +0900

    Title: 블럭의 총 둘레, Time: 193ms, Memory: 33MB, Status: Passed - CodeTree

--- 240122/블럭의 총 둘레/total-perimeter-of-blocks.py
+++ 240122/블럭의 총 둘레/total-perimeter-of-blocks.py
@@ -10,16 +10,18 @@ def input():
     return sys.stdin.readline().rstrip('\n')
 
 # [B] dfs
-def dfs(i, j, is_block, cnt_flag):
+def dfs(i, j):
     global answer
     visited[i][j] = True
-    if cnt_flag:
-        answer += cnt[i][j] if is_block else -cnt[i][j]
     for di, dj in delta:
         ni, nj = i + di, j + dj
         if ni < 0 or nj < 0 or ni >= 102 or nj >= 102: continue
-        if board[ni][nj] != is_block or visited[ni][nj]: continue
-        dfs(ni, nj, is_block, cnt_flag)
+        if visited[ni][nj]: continue
+        # 빈 공간에서 블럭을 발견하면 선분 1개 카운트
+        if board[ni][nj]:
+            answer += 1
+        else:
+            dfs(ni, nj)
 
 # [Main] 
 if __name__ == "__main__":
@@ -32,32 +34,11 @@ if __name__ == "__main__":
     for i, j in blocks:
         board[i][j] = True
 
-    # [3] 인접 블록이 몇개인지 카운트
-    cnt = [[4] * 102 for _ in range(102)]
-    delta = ((-1, 0), (1, 0), (0, -1), (0, 1))
-    for i in range(1, 101):
-        for j in range(1, 101):
-            for di, dj in delta:
-                ni, nj = i + di, j + dj
-                if ni < 1 or nj < 1 or ni >= 101 or nj >= 101: continue
-                if board[i][j] != board[ni][nj]: continue
-                cnt[i][j] -= 1
-
-    # [4] 블럭 외부의 빈칸 체크
+    # [3] dfs 탐색
     visited = [[False] * 102 for _ in range(102)]
-    dfs(0, 0, False, False)
-
-    # [5] 블럭의 외곽선 카운트
+    delta = ((-1, 0), (1, 0), (0, -1), (0, 1))
     answer = 0
-    for i, j in blocks:
-        if visited[i][j]: continue
-        dfs(i, j, True, True)
-
-    # [6] 블럭 내부 빈칸 체크
-    for i in range(102):
-        for j in range(102):
-            if visited[i][j]: continue
-            dfs(i, j, False, True)
+    dfs(0, 0)  # 1 ~ 100을 벗어난 위치에서 탐색 시작
     
-    # [6] 출력
+    # [4] 출력
     print(answer)

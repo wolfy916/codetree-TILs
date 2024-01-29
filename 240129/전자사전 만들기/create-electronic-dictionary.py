36d63e36b02709595d1373acba72e0b051b4dbfe
TAEHYOUNG <109320678+wolfy916@users.noreply.github.com>
2024-01-29 10:16:50 +0900

    Title: 전자사전 만들기, Time: 118ms, Memory: 25MB, Status: Wrong Answer - CodeTree

---
+++ 240129/전자사전 만들기/create-electronic-dictionary.py
@@ -0,0 +1,37 @@
+'''
+전자사전 만들기 - 코드트리 골드4
+분류 : 전처리
+'''
+import sys
+
+# [A] 입력함수 초기화
+def input():
+    return sys.stdin.readline().rstrip('\n')
+
+# [Main]
+if __name__ == "__main__":
+    # [1] 데이터 입력
+    N, T = map(int, input().split())
+    words = [input() for _ in range(N)]
+    orders = [tuple(input().split()) for _ in range(T)]
+
+    # [2] 데이터 초기화
+    idx = {words[i]: i + 1 for i in range(N)}
+    ref = dict()
+
+    # [3] 사전 기록
+    words.sort()
+    for word in words:
+        for i in range(1, len(word)):
+            if ref.get(word[:i]):
+                ref[word[:i]].append(idx[word])
+            else:
+                ref[word[:i]] = [idx[word]]
+
+    # [4] 결과 출력
+    for n, word in orders:
+        n = int(n)
+        if not ref.get(word) or len(ref[word]) < n:
+            print(-1)
+        else:
+            print(ref[word][n - 1])

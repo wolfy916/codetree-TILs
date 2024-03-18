a49adc7949cc48a57927f4be3bba8a587704fffc
TAEHYOUNG <109320678+wolfy916@users.noreply.github.com>
2024-03-18 19:05:19 +0900

    Title: 건물 최소개수, Time: 184ms, Memory: 26MB, Status: Wrong Answer - CodeTree

--- 240318/건물 최소개수/minimum-number-of-building.py
+++ 240318/건물 최소개수/minimum-number-of-building.py
@@ -6,24 +6,23 @@ def input():
 if __name__ == "__main__":
     n = int(input())
     coords = [tuple(map(int, input().split())) for _ in range(n)]
-    stack = []
+    ref = dict()
     answer = 0
 
     for x, y in coords:
-        if len(stack) < 1:
-            answer += 1
+        keys = ref.keys()
+        if len(keys) > 0:
+            for key in sorted(keys, reverse=True):
+                if key > y:
+                    del ref[key]
+                    answer += 1
+                else:
+                    break
+            if not ref.get(y) and y > 0:
+                ref[y] = True
+        else:
+            ref[y] = True
 
-        while stack:
-            if stack[-1] < y:
-                answer += 1
-                break
-            else:
-                stack.pop()
-
-        if y > 0:
-            stack.append(y)
-    
-    if len(stack) > 0:
-        answer += 1
+    answer += len(ref.keys())
 
     print(answer)
